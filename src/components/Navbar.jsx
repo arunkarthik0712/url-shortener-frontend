e07d1a08413defa811368c4240bcfb8e0dd3b4da
arunkarthik0712 <arunkarthik0710@gmail.com>
2024-08-04 12:41:57 +0530

    components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Navbar = ({ isLoggedIn, handleLogout }) => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    handleLogout();
+    navigate("/login");
+  };
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <Link className="navbar-brand" to="/">
+        URL Shortener
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          {isLoggedIn ? (
+            <>
+              <li className="nav-item">
+                <Link className="nav-link link" to="/dashboard">
+                  Dashboard
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link link" to="/shorten">
+                  Shorten URL
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link link" onClick={logout} to="/">
+                  Logout
+                </Link>
+              </li>
+            </>
+          ) : (
+            <></>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
